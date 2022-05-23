764dea17a52615e774f8f42ef49cd010208401a5
pavan0409 <pavanchitguppa@gmail.com>
2022-05-23 16:47:59 +0530

    first commit

---
+++ designingform.js
@@ -0,0 +1,13 @@
+function onsubmit(){
+var name = document.getElementById("name");
+var Email = document.getElementById("Email");
+var Phone = document.getElementById("Phone");
+var Date = document.getElementById("Date");
+var time = document.getElementById("time");
+
+console.log(Name.value);
+console.log(Email.value);
+console.log(Phone.value);
+console.log(Date.value);
+console.log(time.value);
+}
